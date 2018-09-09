fad2926383d3f301fe05d4d84ae96005755f776a
Yaqin Shurrab <yshurrab@gmail.com>
2018-09-09 15:58:44 +0300

    Update annual_accrued_vacation.py

--- erpnext/hr/doctype/annual_accrued_vacation/annual_accrued_vacation.py
+++ erpnext/hr/doctype/annual_accrued_vacation/annual_accrued_vacation.py
@@ -10,7 +10,54 @@ class AnnualAccruedVacation(Document):
 	def before_insert(self):
 		self.one_day_fee = self.basic_salary / self.monthly_duty_days
 		self.total_balance = self.remaining_vacation_days * self.one_day_fee + self.bonus_award
+		if not self.payment_account:
+            		frappe.msgprint(
+				_("Please set Payment Account"),
+				title="Error", indicator="red"
+			    )
+	
+	def get_default_payroll_payable_account(self):
+		payroll_payable_account = frappe.db.get_value("Company",
+			{"company_name": self.company}, "default_payroll_payable_account")
+		if not payroll_payable_account:
+			frappe.throw(_("Please set Default Payment Account"))
 
+		return payroll_payable_account
+	
+	def make_payment_entry(self):
+		self.check_permission('write')
+
+		default_payroll_payable_account = self.get_default_payroll_payable_account()
+		precision = frappe.get_precision("Journal Entry Account", "debit_in_account_currency")
+
+		if self.total_balance:
+		    journal_entry = frappe.new_doc('Journal Entry')
+		    journal_entry.voucher_type = 'Bank Entry'
+		    journal_entry.user_remark = _('Payment of accrued vacation from {0} to {1}') \
+			.format(self.year_start_date, self.year_end_date)
+		    journal_entry.company = self.company
+		    journal_entry.posting_date = self.posting_date
+
+		    payment_amount = flt(self.total_balance, precision)
+
+		    journal_entry.set("accounts", [
+			{
+			    "account": self.payment_account,
+			    "credit_in_account_currency": payment_amount
+			},
+			{
+			    "account": default_payroll_payable_account,
+			    "debit_in_account_currency": payment_amount,
+			    "reference_type": self.doctype,
+			    "reference_name": self.name
+			}
+		    ])
+		    return journal_entry.as_dict()
+		else:
+		    frappe.msgprint(
+			_("There are no Credit for the employee to process."),
+			title="Error", indicator="red"
+		    )
 		
 
 def get_accrued_vacation_bank_entries(accrued_vacation_name):
