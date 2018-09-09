289f0535fe0be395d4ae8584d056edffb1880de3
Yaqin Shurrab <yshurrab@gmail.com>
2018-09-09 16:05:16 +0300

    Update annual_accrued_vacation.py

--- erpnext/hr/doctype/annual_accrued_vacation/annual_accrued_vacation.py
+++ erpnext/hr/doctype/annual_accrued_vacation/annual_accrued_vacation.py
@@ -6,6 +6,7 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 from frappe import _, msgprint
+from frappe.utils import flt
 
 
 class AnnualAccruedVacation(Document):
