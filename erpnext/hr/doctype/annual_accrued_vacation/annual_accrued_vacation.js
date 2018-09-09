f9f690c1f08e5cdab2ebafa4d2534f43e95252bb
Yaqin Shurrab <yshurrab@gmail.com>
2018-09-09 14:27:36 +0300

    Update annual_accrued_vacation.js

--- erpnext/hr/doctype/annual_accrued_vacation/annual_accrued_vacation.js
+++ erpnext/hr/doctype/annual_accrued_vacation/annual_accrued_vacation.js
@@ -3,7 +3,9 @@
 
 frappe.ui.form.on('Annual Accrued Vacation', {
 	refresh: function(frm) {
-
+		if(frm.doc.docstatus === 1){
+			frm.events.add_bank_entry_button(frm);
+		}
 	},
 	setup: function (frm) {
 		frm.set_query("payment_account", function () {
@@ -31,5 +33,41 @@ frappe.ui.form.on('Annual Accrued Vacation', {
 				}
 			};
 		});
+	},
+	add_bank_entry_button: function(frm) {
+		frappe.call({
+			method: 'erpnext.hr.doctype.payroll_entry.payroll_entry.payroll_entry_has_bank_entries',
+			args: {
+				'name': frm.doc.name
+			},
+			callback: function(r) {
+				if (r.message && !r.message.submitted) {
+					frm.add_custom_button("Bank Entry",
+						function() {
+							make_bank_entry(frm);
+						},
+						__('Make')
+					);
+					frm.page.set_inner_btn_group_as_primary(__('Make'));
+				}
+			}
+		});
 	}
 });
+
+let make_bank_entry = function (frm) {
+	var doc = frm.doc;
+	if (doc.company && doc.start_date && doc.end_date) {
+		return frappe.call({
+			doc: cur_frm.doc,
+			method: "make_payment_entry",
+			callback: function (r) {
+				if (r.message)
+					var doc = frappe.model.sync(r.message)[0];
+				frappe.set_route("Form", doc.doctype, doc.name);
+			}
+		});
+	} else {
+		frappe.msgprint(__("Company, From Date and To Date is mandatory"));
+	}
+};
