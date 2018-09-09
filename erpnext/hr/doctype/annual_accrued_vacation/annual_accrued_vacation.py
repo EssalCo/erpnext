3d44299be4eef740a8432af119de54969531aa05
Yaqin Shurrab <yshurrab@gmail.com>
2018-09-09 13:09:38 +0300

    Update annual_accrued_vacation.py

--- erpnext/hr/doctype/annual_accrued_vacation/annual_accrued_vacation.py
+++ erpnext/hr/doctype/annual_accrued_vacation/annual_accrued_vacation.py
@@ -7,4 +7,6 @@ import frappe
 from frappe.model.document import Document
 
 class AnnualAccruedVacation(Document):
-	pass
+	before_insert(self):
+		self.one_day_fee = self.basic_salary / self.monthly_duty_days
+		self.total_balance = self.remaining_vacation_days * self.one_day_fee + self.bonus_award
