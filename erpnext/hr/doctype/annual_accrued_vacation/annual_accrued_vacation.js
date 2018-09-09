faa3addb6d7fc4b6cd0b44b9924729bf5fbaaa68
Yaqin Shurrab <yshurrab@gmail.com>
2018-09-09 14:15:46 +0300

    Update annual_accrued_vacation.js

--- erpnext/hr/doctype/annual_accrued_vacation/annual_accrued_vacation.js
+++ erpnext/hr/doctype/annual_accrued_vacation/annual_accrued_vacation.js
@@ -4,5 +4,32 @@
 frappe.ui.form.on('Annual Accrued Vacation', {
 	refresh: function(frm) {
 
+	},
+	setup: function (frm) {
+		frm.set_query("payment_account", function () {
+			var account_types = ["Bank", "Cash"];
+			return {
+				filters: {
+					"account_type": ["in", account_types],
+					"is_group": 0,
+					"company": frm.doc.company
+				}
+			};
+		}),
+		frm.set_query("cost_center", function () {
+			return {
+				filters: {
+					"is_group": 0,
+					company: frm.doc.company
+				}
+			};
+		}),
+		frm.set_query("project", function () {
+			return {
+				filters: {
+					company: frm.doc.company
+				}
+			};
+		});
 	}
 });
