3e05ab5e76f39bbaef39e290ed1ee1be68bd0b08
saradiab <sara.emad.diab@gmail.com>
2018-09-12 00:48:27 +0300

    trace error

--- erpnext/utilities/hijri_date.py
+++ erpnext/utilities/hijri_date.py
@@ -8,14 +8,14 @@ def convert_to_hijri(date):
         return ""
     if "." in str(date):
         date = str(date).split(".")[0]
-    # day = int(datetime.strptime(str(date), '%Y-%m-%d').strftime('%d'))
-    # month = int(datetime.strptime(str(date), '%Y-%m-%d').strftime('%m'))
-    # year = int(datetime.strptime(str(date), '%Y-%m-%d').strftime('%Y'))
-    #
-    # hijri_date = HijriDate(year, month, day, gr=True)
-    # hijri_date = "{0}-{1}-{2} \n {3}".format(int(hijri_date.day),
-    #                                   int(hijri_date.month),
-    #                                   int(hijri_date.year),
-    #                                   hijri_date.month_name,
-    #                                   )
-    return date
+    day = int(datetime.strptime(str(date), '%Y-%m-%d').strftime('%d'))
+    month = int(datetime.strptime(str(date), '%Y-%m-%d').strftime('%m'))
+    year = int(datetime.strptime(str(date), '%Y-%m-%d').strftime('%Y'))
+
+    hijri_date = HijriDate(year, month, day, gr=True)
+    hijri_date = "{0}-{1}-{2} \n {3}".format(int(hijri_date.day),
+                                      int(hijri_date.month),
+                                      int(hijri_date.year),
+                                      hijri_date.month_name,
+                                      )
+    return hijri_date
