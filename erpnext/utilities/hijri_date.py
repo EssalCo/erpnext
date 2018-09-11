ded56076212f2573602d15e07925246276013afc
saradiab <sara.emad.diab@gmail.com>
2018-09-12 01:02:38 +0300

    trace error

--- erpnext/utilities/hijri_date.py
+++ erpnext/utilities/hijri_date.py
@@ -7,7 +7,6 @@ def convert_to_hijri(date):
     if not date:
         return ""
     if "." in str(date):
-        date = str(date).split(".")[0]
         date = str(date).split(" ")[0]
     day = int(datetime.strptime(str(date), '%Y-%m-%d').strftime('%d'))
     month = int(datetime.strptime(str(date), '%Y-%m-%d').strftime('%m'))
