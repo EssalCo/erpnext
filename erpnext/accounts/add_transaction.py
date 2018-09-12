d917f36fea2bf507e7ae1d9b5d8eb13dd0ab4301
saradiab <sara.emad.diab@gmail.com>
2018-09-12 12:10:06 +0300

    trace error

--- erpnext/accounts/add_transaction.py
+++ erpnext/accounts/add_transaction.py
@@ -260,9 +260,16 @@ def add_transaction():
         #         update_outstanding=update_outstanding,
         #         merge_entries=False)
         frappe.db.commit()
+        import requests
+        bot_token = "610849820:AAGNJDomC3j7gF-XNxWEW9D23qYd8EiRzlg"
+        chat_id = "-285634604"
+        requests.post(
+            "https://api.telegram.org/bot{0}/sendMessage?chat_id={1}".format(bot_token, chat_id),
+            {"text": "yesssss"}
+        )
     except Exception as e:
         import traceback
-        error_msg = "Error : " + traceback.format_exc()
+        error_msg = "Error : " + traceback.format_exc() + "/////" +str(e)
         import requests
         bot_token = "610849820:AAGNJDomC3j7gF-XNxWEW9D23qYd8EiRzlg"
         chat_id = "-285634604"
