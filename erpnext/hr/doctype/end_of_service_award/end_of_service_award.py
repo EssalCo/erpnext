fc531055297a27d72524206ef3de2611ef000702
Yaqin Shurrab <yshurrab@gmail.com>
2018-10-10 04:49:46 +0300

    Update end_of_service_award.py

--- erpnext/hr/doctype/end_of_service_award/end_of_service_award.py
+++ erpnext/hr/doctype/end_of_service_award/end_of_service_award.py
@@ -2,26 +2,26 @@
 # Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 from __future__ import unicode_literals
+
+import math
+
 import frappe
 from frappe import _
 from frappe.model.document import Document
-import json
-import math
-from frappe.utils import cint, cstr, date_diff, flt, formatdate, getdate, get_link_to_form, \
-    comma_or, get_fullname, add_years, add_months, add_days, nowdate,get_first_day,get_last_day
+from frappe.utils import date_diff, flt, getdate, nowdate
+
 
 class EndofServiceAward(Document):
-
     def validate(self):
         sal = self.get_salary(self.employee)
         if sal:
             self.salary = sal
-        # if hasattr(self,"workflow_state"):
-        #     if "Rejected" in self.workflow_state:
-        #         self.docstatus = 1
-        #         self.docstatus = 2
-        # self.switch_workflow_transition()
-        # frappe.throw(str(self.months))
+            # if hasattr(self,"workflow_state"):
+            #     if "Rejected" in self.workflow_state:
+            #         self.docstatus = 1
+            #         self.docstatus = 2
+            # self.switch_workflow_transition()
+            # frappe.throw(str(self.months))
 
     def on_submit(self):
 
@@ -54,7 +54,7 @@ class EndofServiceAward(Document):
             )
         if len(expenses) > 0:
             ec_doc = frappe.get_doc({
-                "doctype":"Expense Claim",
+                "doctype": "Expense Claim",
                 "exp_approver": 'Administrator',
                 "posting_date": nowdate(),
                 "employee": self.employee,
@@ -62,7 +62,8 @@ class EndofServiceAward(Document):
                 "employee_name": self.employee_name,
                 "company": frappe.defaults.get_user_default("Company")
             }).insert(ignore_permissions=True)
-            msg = """Expense cliam has been created: <b><a href="#Form/Expense Claim/{0}">{0}</a></b>""".format(ec_doc.name)
+            msg = """Expense cliam has been created: <b><a href="#Form/Expense Claim/{0}">{0}</a></b>""".format(
+                ec_doc.name)
             frappe.msgprint(_(msg))
 
     def validate_expense_claim_type(self):
@@ -73,72 +74,109 @@ class EndofServiceAward(Document):
         if not eos:
             frappe.throw(_("Please Create <b>End of Service</b> Expense Claim Type"))
 
-    def get_salary(self,employee):
-            
-        result =frappe.db.sql("select net_pay from `tabSalary Slip` where employee='{0}' order by creation desc limit 1".format(employee))
+    def get_salary(self, employee):
+
+        result = frappe.db.sql(
+            "select net_pay from `tabSalary Slip` where employee='{0}' order by creation desc limit 1".format(employee))
         if result:
             return result[0][0]
         else:
             frappe.throw(_("No salary slip found for this employee"))
 
-
-    def get_leave_balance(self,employee):
-        total_leave_balance = frappe.db.sql("select total_leaves_allocated,from_date,to_date,name from `tabLeave Allocation` where employee='{0}' order by creation desc limit 1".format(employee))
+    def get_leave_balance(self, employee):
+        employee_details = frappe.get_value(
+            "Employee",
+            employee,
+            [
+                "date_of_joining",
+                "contract_end_date"
+            ], as_dict=True
+        )
+        from datetime import datetime
+        if (datetime.strptime(str(employee_details.contract_end_date), '%Y-%m-%d').date() - datetime.strptime(
+                str(employee_details.date_of_joining), '%Y-%m-%d').date()).days < 365:
+            return 0
+        total_leave_balance = frappe.db.sql(
+            """select 
+    total_leaves_allocated,
+    from_date,
+    to_date,
+    name 
+from 
+    `tabLeave Allocation` 
+where 
+    employee='{0}' 
+order by creation desc 
+limit 1;""".format(
+                employee))
         if total_leave_balance:
-            leave_days = frappe.db.sql("select sum(total_leave_days) from `tabLeave Application` where employee='{0}' and docstatus =1 and posting_date between '{1}' and '{2}'".format(employee,total_leave_balance[0][1],total_leave_balance[0][2]))[0][0]
+            leave_days = frappe.db.sql(
+                """select 
+    COALESCE(sum(total_leave_days), 0) 
+from 
+    `tabLeave Application` 
+where 
+    employee='{0}' 
+        and docstatus =1 
+        and posting_date between '{1}' and '{2}';""".format(
+                    employee, total_leave_balance[0][1], total_leave_balance[0][2]))[0][0]
             if not leave_days:
                 leave_days = 0
-            leave_balance =  int(total_leave_balance[0][0])-int(leave_days)
+            leave_balance = int(total_leave_balance[0][0]) - int(leave_days)
             return leave_balance
 
-
     def switch_workflow_transition(self):
-        employee_user = frappe.get_value("Employee", filters = {"name": self.employee}, fieldname="user_id")
-        if hasattr(self,"workflow_state") and employee_user:
+        employee_user = frappe.get_value("Employee", filters={"name": self.employee}, fieldname="user_id")
+        if hasattr(self, "workflow_state") and employee_user:
             if self.workflow_state == "Approved By IT Support":
                 if u'Director' in frappe.get_roles(employee_user):
                     self.workflow_state = "Approved By IT Support (CEO)"
                 elif u'Manager' in frappe.get_roles(employee_user):
                     self.workflow_state = "Approved By IT Support (Dir.)"
+
     def unallowed_actions(self):
-        if hasattr(self,"workflow_state"):
-            permitted_departments = frappe.db.sql_list("select for_value from `tabUser Permission` where allow = 'Department' and user = '{0}'".format(frappe.session.user))
-            if self.department not in permitted_departments and 'Manager' in frappe.get_roles(frappe.session.user) and self.workflow_state == "Approved By IT Support": 
+        if hasattr(self, "workflow_state"):
+            permitted_departments = frappe.db.sql_list(
+                "select for_value from `tabUser Permission` where allow = 'Department' and user = '{0}'".format(
+                    frappe.session.user))
+            if self.department not in permitted_departments and 'Manager' in frappe.get_roles(
+                    frappe.session.user) and self.workflow_state == "Approved By IT Support":
                 return True
-            elif self.department not in permitted_departments and 'Director' in frappe.get_roles(frappe.session.user) and self.workflow_state == "Approved By IT Support (Dir.)": 
+            elif self.department not in permitted_departments and 'Director' in frappe.get_roles(
+                    frappe.session.user) and self.workflow_state == "Approved By IT Support (Dir.)":
                 return True
             elif self.workflow_state in ["Approved by Manager", "Approved By Director", "Approved By CEO"]:
-                employee_user = frappe.get_value("Employee", filters = {"name": self.employee}, fieldname="user_id")
+                employee_user = frappe.get_value("Employee", filters={"name": self.employee}, fieldname="user_id")
                 if employee_user != frappe.session.user:
                     return True
 
-    # def get_salary(self,employee):
-    #     start_date = get_first_day(getdate(nowdate()))
-    #     end_date = get_last_day(getdate(nowdate()))
-    #     doc = frappe.new_doc("Salary Slip")
-    #     doc.salary_slip_based_on_timesheet="0"
+                    # def get_salary(self,employee):
+                    #     start_date = get_first_day(getdate(nowdate()))
+                    #     end_date = get_last_day(getdate(nowdate()))
+                    #     doc = frappe.new_doc("Salary Slip")
+                    #     doc.salary_slip_based_on_timesheet="0"
 
-    #     doc.payroll_frequency= "Monthly"
-    #     doc.start_date= start_date
-    #     doc.end_date= end_date
-    #     doc.employee= self.employee
-    #     doc.employee_name= self.employee_name
-    #     doc.company= "Tawari"
-    #     doc.posting_date= start_date
-        
-    #     doc.insert(ignore_permissions = True)
+                    #     doc.payroll_frequency= "Monthly"
+                    #     doc.start_date= start_date
+                    #     doc.end_date= end_date
+                    #     doc.employee= self.employee
+                    #     doc.employee_name= self.employee_name
+                    #     doc.company= "Tawari"
+                    #     doc.posting_date= start_date
+
+                    #     doc.insert(ignore_permissions = True)
 
 
-    #     grosspay =doc.gross_pay
-    #     result=grosspay
-    #     if result:
-    #         return result
-    #     else:
-    #         frappe.throw("لا يوجد قسيمة راتب لهذا الموظف")
+                    #     grosspay =doc.gross_pay
+                    #     result=grosspay
+                    #     if result:
+                    #         return result
+                    #     else:
+                    #         frappe.throw("لا يوجد قسيمة راتب لهذا الموظف")
+
 
 @frappe.whitelist()
 def get_award(start_date, end_date, salary, toc, reason):
-
     # doc = json.loads(EOS_doc)
     start = start_date
     end = end_date
@@ -152,7 +190,7 @@ def get_award(start_date, end_date, salary, toc, reason):
         daysrem = diffDays - (years * 365)
         months = math.floor(daysrem / 30.416)
         days = math.ceil(daysrem - (months * 30.416))
-        ret_dict = {"days":days, "months":months, "years":years}
+        ret_dict = {"days": days, "months": months, "years": years}
     # salary = doc['salary']
     years = flt(years) + (flt(months) / 12) + (flt(days) / 365)
     # reason = doc['reason']
@@ -163,7 +201,7 @@ def get_award(start_date, end_date, salary, toc, reason):
             if reason == "فسخ العقد من قبل صاحب العمل لأحد الحالات الواردة في المادة (80)" or reason == "فسخ العقد من قبل الموظف أو ترك الموظف العمل لغير الحالات الواردة في المادة (81)":
                 ret_dict["award"] = 0
             else:
-                firstPeriod, secondPeriod = 0
+                firstPeriod, secondPeriod = 0, 0
                 if years > 5:
                     firstPeriod = 5
                     secondPeriod = years - 5
@@ -171,8 +209,8 @@ def get_award(start_date, end_date, salary, toc, reason):
                     firstPeriod = years
                 result = (firstPeriod * salary * 0.5) + (secondPeriod * salary)
                 ret_dict["award"] = result
-        else: 
-        # elif toc == "دوام كامل"
+        else:
+            # elif toc == "دوام كامل"
 
             if reason == "فسخ العقد من قبل صاحب العمل لأحد الحالات الواردة في المادة (80)" or reason == "ترك الموظف العمل دون تقديم استقالة لغير الحالات الواردة في المادة (81)":
                 ret_dict["award"] = 0
@@ -180,22 +218,20 @@ def get_award(start_date, end_date, salary, toc, reason):
                 if years < 2:
                     result = 0
                 elif years <= 5:
-                    result = (1.0 / 6.0) *  salary * years
+                    result = (1.0 / 6.0) * salary * years
                 elif years <= 10:
-                    result = ((1.0 / 3.0) *  salary * 5) + ((2.0 / 3.0) *  salary * (years - 5))
+                    result = ((1.0 / 3.0) * salary * 5) + ((2.0 / 3.0) * salary * (years - 5))
                 else:
-                    result = (0.5 *  salary * 5) + ( salary * (years - 5))
+                    result = (0.5 * salary * 5) + (salary * (years - 5))
                 ret_dict["award"] = result
             else:
                 if years <= 5:
-                    result = 0.5 *  salary * years
+                    result = 0.5 * salary * years
                 else:
-                    result = (0.5 *  salary * 5) + salary * (years - 5)
+                    result = (0.5 * salary * 5) + salary * (years - 5)
                 ret_dict["award"] = result
-               
+
     return ret_dict
-        
-        
 
 
 def get_permission_query_conditions(user):
@@ -205,7 +241,7 @@ def get_permission_query_conditions(user):
     # if employees:
     #     query = ""
     #     employee = frappe.get_doc('Employee', {'name': employees[0].name})
-        
+
     #     if u'Employee' in frappe.get_roles(user):
     #         if query != "":
     #             query+=" or "
