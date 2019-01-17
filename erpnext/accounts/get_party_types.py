fa862297376a962baad24b0ce5b33481dc499025
Yaqin Shurrab <yshurrab@gmail.com>
2019-01-17 02:15:49 +0200

    Create get_party_types.py

---
+++ erpnext/accounts/get_party_types.py
@@ -0,0 +1,20 @@
+
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import frappe
+
+
+@frappe.whitelist(allow_guest=True)
+def get_party_types():
+    try:
+        party_types = [temp.name for temp in frappe.get_all("Party Type",
+                                       filters=dict(
+                                       ),
+                                       ignore_permissions=True,
+                                       ignore_ifnull=True)]
+        
+    except Exception as e:
+        return dict(status=False, message=str(e))
+    return dict(status=True, message="Success", party_types=companies)
+
