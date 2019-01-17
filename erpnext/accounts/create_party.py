7e9e6226d1d02190f4f5c7d50fc43225741fc23b
Yaqin Shurrab <yshurrab@gmail.com>
2019-01-17 02:25:19 +0200

    Create create_party.py

---
+++ erpnext/accounts/create_party.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from datetime import datetime
+
+import frappe
+
+
+@frappe.whitelist(allow_guest=True)
+def create_party():
+    # 'party_type',
+    # 'party_name' ,
+    # 'party_phone',
+    # 'party_email'
+    
+    try:
+        
+        party = frappe.get_doc(
+            dict(
+                doctype=party_type
+            )
+        )
+        party.insert(ignore_permissions=True)
+        frappe.db.commit()
+
+        return dict(status=True, message="Party is added successfully")
+    except Exception as e:
+        return dict(status=False, message=str(e))
+
