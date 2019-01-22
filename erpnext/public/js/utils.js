7f2b6ded00cf1640ef8dfe52ea40db629b1777ff
Ola Akeela <ola.akeela@gmail.com>
2019-01-22 21:00:58 +0200

    fix conflict

--- erpnext/public/js/utils.js
+++ erpnext/public/js/utils.js
@@ -185,6 +185,7 @@ $.extend(erpnext.utils, {
 	},
 	get_tree_options: function(option) {
 		// get valid options for tree based on user permission & locals dict
+		return localStorage.getItem("session_company");
 		let unscrub_option = frappe.model.unscrub(option);
 		let user_permission = frappe.defaults.get_user_permissions();
 		if(user_permission && user_permission[unscrub_option]) {
@@ -194,6 +195,7 @@ $.extend(erpnext.utils, {
 		}
 	},
 	get_tree_default: function(option) {
+		return localStorage.getItem("session_company");
 		// set default for a field based on user permission
 		let options = this.get_tree_options(option);
 		if(options.includes(frappe.defaults.get_default(option))) {
