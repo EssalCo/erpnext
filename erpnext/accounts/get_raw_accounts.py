59743db4c8c6412d23a413ae803d82a5a668aea2
Yaqin Shurrab <yshurrab@gmail.com>
2019-01-31 00:29:32 +0200

    Update get_raw_accounts.py

--- erpnext/accounts/get_raw_accounts.py
+++ erpnext/accounts/get_raw_accounts.py
@@ -1,1 +1,21 @@
 
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import frappe
+
+
+@frappe.whitelist(allow_guest=True)
+def get_raw_accounts():
+    try:
+        accounts = frappe.get_all(
+          "Account",
+          fields=["*"],
+         filters=dict(
+         ),
+         ignore_permissions=True,
+         ignore_ifnull=True)]
+        
+    except Exception as e:
+        return dict(status=False, message=str(e))
+return dict(status=True, message="Success", accounts=accounts)
