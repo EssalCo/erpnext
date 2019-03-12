ad707a1b755d338931d8ec9bc7b0283d97809c0e
Yaqin Shurrab <yshurrab@gmail.com>
2019-03-12 14:33:18 +0200

    Update migrate_users.py

--- erpnext/accounts/migrate_users.py
+++ erpnext/accounts/migrate_users.py
@@ -8,7 +8,7 @@ def execute():
     response = requests.request("POST", url)
     print response
     result = json.loads(response.text)
-    users = result['users']
+    users = result['message']['users']
 
     print len(users)
     for user in users:
@@ -23,9 +23,9 @@ def execute():
         new_doc.insert(ignore_permissions=1)
 
         frappe.db.sql("""UPDATE `tabUser` SET `name` = %s WHERE `name` = %s""", (user.name, new_doc.name))
-        print new_doc.name, user.name
+        print new_doc.name, user['name']
 
-    employees = result['employees']
+    employees = result['message']['employees']
 
     print len(employees)
     for employee in employees:
@@ -39,5 +39,45 @@ def execute():
         new_doc = frappe.get_doc(new_order_dict)
         new_doc.insert(ignore_permissions=1)
 
-        frappe.db.sql("""UPDATE `tabEmployee` SET `name` = %s WHERE `name` = %s""", (employee.name, new_doc.name))
-        print new_doc.name, employee.name
+        print new_doc.name, employee['name']
+        
+        
+# import frappe
+# import requests
+# import json
+# 
+# 
+# def execute():
+#     url = 'https://serafico.s1.essal.co/api/method/erpnext.accounts.get_companies.get_users'
+#     response = requests.request("POST", url)
+#     print response
+#     result = json.loads(response.text)
+#     types = result['message']['users']
+# 
+#     print len(types)
+#     for user in types:
+# 
+#         new_order_dict = dict()
+#         for key in user:
+#             new_order_dict[key] = user[key]
+# 
+#         new_doc = frappe.get_doc(new_order_dict)
+#         new_doc.insert(ignore_permissions=1)
+#     frappe.db.commit()
+#     employees = result['message']['employees']
+# 
+#     print len(employees)
+#     for employee in employees:
+# 
+#         new_order_dict = dict()
+#         for key in employee:
+#             new_order_dict[key] = employee[key]
+# 
+#         new_order_dict["doctype"] = "Employee"
+# 
+#         new_doc = frappe.get_doc(new_order_dict)
+#         new_doc.insert(ignore_permissions=1)
+# 
+#         frappe.db.sql("""UPDATE `tabEmployeer` SET `name` = %s WHERE `name` = %s""", (employee['name'], new_doc.name))
+#         print new_doc.name, employee['name']
+
