a1c77b539972fabcbdb79ddd08b5ae795659359c
Yaqin Shurrab <yshurrab@gmail.com>
2019-03-17 15:57:33 +0200

    Update add_transaction.py

--- erpnext/accounts/add_transaction.py
+++ erpnext/accounts/add_transaction.py
@@ -385,7 +385,9 @@ def add_transaction_v2():
 
         data = frappe.form_dict.get('data')
         send_msg_telegram(str(data))
-
+        if isinstance(data, basestring):
+            import json
+            data = json.loads(data)
         contract_id = data.get('contract_id')
         date = data.get('date')
         transactions_list = data.get('transactions_list', '[]')
