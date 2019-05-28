2680423253ce84eb403740c9f6bf2f929e9112b6
Yaqin Shurrab <yshurrab@gmail.com>
2019-05-28 23:12:54 +0300

    Update journal_entry.py

--- erpnext/accounts/doctype/journal_entry/journal_entry.py
+++ erpnext/accounts/doctype/journal_entry/journal_entry.py
@@ -446,7 +446,7 @@ class JournalEntry(AccountsController):
 						"credit_in_account_currency": flt(d.credit_in_account_currency, d.precision("credit_in_account_currency")),
 						"against_voucher_type": d.reference_type,
 						"against_voucher": d.reference_name,
-						"remarks": self.remark,
+						"remarks": d.journal_note or self.remark,
 						"cost_center": d.cost_center,
 						"project": d.project
 					})
