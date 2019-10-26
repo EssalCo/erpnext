943b96696e5ba3b357a5ee9607473203c1a26985
yshurrab <yshurrab@gmail.com>
2019-10-26 13:54:58 +0200

    add script for cost center

---
+++ erpnext/cron_jobs/add_cost_centers.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import sys
+
+import frappe
+
+sys.path.append('../..')
+import csv
+from frappe.utils.file_manager import get_file_path
+from datetime import datetime
+from umalqurra.hijri_date import HijriDate
+
+
+def execute():
+    file = "/private/files/villas_no.csv"
+    cost_centers = ["@ قسم السباكة - TDCO", "@ قسم الكهرباء - TDCO", "@قسم الانشائي - TDCO"]
+    cost_centers_names = ["قسم السباكة", "قسم الكهرباء", "قسم الانشائي"]
+    current_file = get_file_path(file)
+
+    company = frappe.get_doc(
+        "Company",
+        dict(
+            company_name="شركة ابعاد فنية للمقاولات"
+        )
+    )
+
+    with open(current_file, 'rb') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=str(","), quotechar=str("|"))
+        for row in spamreader:
+            # print row
+            try:
+                int(row[0])
+            except:
+                continue
+            villa = str(row[0])
+            print(villa)
+            doc = frappe.get_doc(
+                        dict(
+                            doctype="Cost Center",
+                            cost_center_name="{0} - فيلا - {1}".format(
+                                cost_centers_names[0],
+                                villa
+                            ),
+                            parent_cost_center=cost_centers[0],
+                            company=company.name,
+                            is_group=0
+                        )
+            )
+            doc.flags.ignore_mandatory = True
+            try:
+                doc.insert(ignore_permissions=True)
+            except frappe.exceptions.DuplicateEntryError:
+                pass
+
+            doc = frappe.get_doc(
+                dict(
+                    doctype="Cost Center",
+                    cost_center_name="{0} - فيلا - {1}".format(
+                        cost_centers_names[1],
+                        villa
+                    ),
+                    parent_cost_center=cost_centers[1],
+                    company=company.name,
+                    is_group=0
+                )
+            )
+            doc.flags.ignore_mandatory = True
+            try:
+                doc.insert(ignore_permissions=True)
+            except frappe.exceptions.DuplicateEntryError:
+                pass
+
+            doc = frappe.get_doc(
+                dict(
+                    doctype="Cost Center",
+                    cost_center_name="{0} - فيلا - {1}".format(
+                        cost_centers_names[2],
+                        villa
+                    ),
+                    parent_cost_center=cost_centers[2],
+                    company=company.name,
+                    is_group=0
+                )
+            )
+            doc.flags.ignore_mandatory = True
+            try:
+                doc.insert(ignore_permissions=True)
+            except frappe.exceptions.DuplicateEntryError:
+                pass
