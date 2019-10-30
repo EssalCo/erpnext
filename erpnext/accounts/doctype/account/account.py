3f995e4dcdc7301b562ed052d6bbf0c43d525d90
yshurrab <yshurrab@gmail.com>
2019-10-30 15:17:32 +0200

    fix

--- erpnext/accounts/doctype/account/account.py
+++ erpnext/accounts/doctype/account/account.py
@@ -205,13 +205,13 @@ class Account(NestedSet):
     WHERE 
         company = %s
             AND parent_account IS NULL;""", (self.company,), as_dict=True)
-                if len(last_existing_serial) == 0:
+                if len(last_existing_serial) == 0 or not last_existing_serial[0].maxi:
                     # last_existing_serial = 0
-                    # next_serial = 1
+                    next_serial = 1
                     next_serial_str = "#1"
                 else:
                     last_existing_serial = last_existing_serial[0].maxi
-                    # next_serial = last_existing_serial + 1
+                    next_serial = last_existing_serial + 1
                     next_serial_str = "#{0}".format(last_existing_serial + 1)
             else:
                 #send_msg_telegram("parent " + str(self.account_serial) + str(self.account_serial_x))
@@ -220,27 +220,33 @@ class Account(NestedSet):
       `tabAccount` WHERE
        account_serial = (
     SELECT 
-        MAX(account_serial) AS maxi
+        MAX(account_serial * 1) AS maxi
     FROM
         tabAccount
     WHERE 
         company = %s
             AND parent_account = %s);""", (self.company, self.parent_account), as_dict=True)
-                parent_serial = frappe.db.get_value(
+                parent_serial, account_serial_x = frappe.db.get_value(
                     "Account",
                     self.parent_account,
-                    "account_serial"
+                    [
+                    "account_serial",
+                        "account_serial_x"
+                        ]
                 )
-                if len(last_existing_serial) == 0:
-                    # last_existing_serial = parent_serial * 10
-                    # next_serial = last_existing_serial + 1
+                if len(last_existing_serial) == 0 or not last_existing_serial[0].account_serial:
+                    last_existing_serial = parent_serial * 100
+                    next_serial = last_existing_serial + 1
                     next_serial_str = "{0}.{1}".format(parent_serial, 1)
                 else:
-                    trimmed_serial = str(last_existing_serial[0].account_serial_x).split(".")[-1]
-                    next_serial_str = "{0}.{1}".format(parent_serial, int(trimmed_serial) + 1)
+                    last_existing_serial = last_existing_serial[0].account_serial
+                    next_serial = last_existing_serial + 1
+
+                    # trimmed_serial = str(last_existing_serial[0].account_serial_x).split(".")[-1]
+                    next_serial_str = "{0}.{1}".format(account_serial_x, next_serial)
             #send_msg_telegram("finish " + str(self.account_serial) + str(self.account_serial_x))
 
-            self.account_serial = int(next_serial_str.replace(".", "").replace("#", ""))
+            self.account_serial = next_serial
             self.account_serial_x = next_serial_str
         except:
             pass
