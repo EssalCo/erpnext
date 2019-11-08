3173a4672cfc9acfe0c6b41adb17e2b9ed13a499
yshurrab <yshurrab@gmail.com>
2019-11-08 23:11:07 +0200

    ipdate script

--- erpnext/cron_jobs/migrate_nahdi_data_to_essal.py
+++ erpnext/cron_jobs/migrate_nahdi_data_to_essal.py
@@ -15,14 +15,10 @@ from umalqurra.hijri_date import HijriDate
 
 def execute():
 
-    frappe.db.sql("""UPDATE
-    tabAccount set name = replace(name, "Nت", "")""")
-
-    return
     payment_details = "/private/files/payment_details.csv"
     payment = "/private/files/payment.csv"
     owners = "/private/files/owners.csv"
-    parent_account = "Debtors - N"
+    parent_account = "50101 - حسابات الدائنون - N"
     print("Starting owners..")
     current_file = get_file_path(owners)
     company = frappe.get_doc(
@@ -39,6 +35,8 @@ def execute():
         ),
         "name"
     )
+
+    cash_account = " 3030601 - نقد - N"
     with open(current_file, 'rb') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=str(","), quotechar=str("|"))
         for row in spamreader:
@@ -68,7 +66,9 @@ def execute():
             doc.insert(ignore_permissions=True)
 
     print("Done owners..")
+
     print("Start Payments..")
+    current_file = get_file_path(payment)
 
     with open(current_file, 'rb') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=str(","), quotechar=str("|"))
@@ -95,10 +95,31 @@ def execute():
             pay_name = row[10]
             receipt_no = row[11]
             total = value_1 + value_2 + value_3 + value_4
+            remark_str = ""
+            if cheq:
+                remark_str = remark_str + "CHEQNO: {0}".format(cheq)
+            if bank:
+                if remark_str:
+                    remark_str += ", "
+                remark_str = remark_str + "BANK: {0}".format(bank)
+            if remark:
+                if remark_str:
+                    remark_str += ", "
+                remark_str = remark_str + "REMARK: {0}".format(remark)
+            if pay_name:
+                if remark_str:
+                    remark_str += ", "
+                remark_str = remark_str + "PAY_NAME: {0}".format(pay_name)
+            if receipt_no:
+                if remark_str:
+                    remark_str += ", "
+                remark_str = remark_str + "RCP_NO: {0}".format(receipt_no)
+
             account_name = frappe.get_value(
                 "Account",
                 dict(
-                    parent=parent_account
+                    parent=parent_account,
+                    name=("like", "%- {0} -%".format(account_no))
                 )
             )
             journal_entry = frappe.get_doc(
@@ -111,7 +132,7 @@ def execute():
                     company=company.name,
                     user_remark=remark,
                     multi_currency=0,
-                    remark="CHEQNO: {0}, BANK: {1}, REMARK: {2}, PAY_NAME: {3}, RCP_NO: {4}".format(cheq, bank, remark, pay_name, receipt_no),
+                    remark=remark_str,
                     bill_date=datetime.now(),
                     third_party_creation=post_date,
                     accounts=[],
@@ -121,162 +142,45 @@ def execute():
             )
 
             journal_entry.append("accounts", dict(
-                account=account_name,
-                party_type=None,
-                party=None,
-                title=description,
+                account=cash_account,
+                against_account=account_name,
+                title=remark,
                 exchange_rate=1,
-                debit_in_account_currency=abs(debit),
-                debit=abs(debit),
-                journal_note=description,
-                credit_in_account_currency=abs(credit),
-                credit=abs(credit),
+                debit_in_account_currency=abs(total),
+                debit=abs(total),
+                journal_note=remark,
+                credit_in_account_currency=abs(0),
+                credit=abs(0),
                 is_advance="No",
                 cost_center=main_cost_center
             ))
             journal_entry.append("accounts", dict(
+                party_type="Company",
+                party=company.name,
                 account=account_name,
-                party_type=None,
-                party=None,
-                title=description,
+                title=remark,
                 exchange_rate=1,
-                debit_in_account_currency=abs(debit),
-                debit=abs(debit),
-                journal_note=description,
-                credit_in_account_currency=abs(credit),
-                credit=abs(credit),
+                debit_in_account_currency=0,
+                debit=0,
+                journal_note=remark,
+                credit_in_account_currency=abs(total),
+                credit=abs(total),
                 is_advance="No",
                 cost_center=main_cost_center
             ))
-    # current_file = get_file_path(cost_centers)
-    # # current_file = current_file.replace("s1.essal.co", "alnamaa.s1.essal.co")
-    # print("Starting Cost Centers..")
-    #
-    # with open(current_file, 'rb') as csvfile:
-    #     spamreader = csv.reader(csvfile, delimiter=str(","), quotechar=str("|"))
-    #     for row in spamreader:
-    #         try:
-    #             serial_no = int(row[0])
-    #         except:
-    #             continue
-    #         cost_center_name = row[1].decode('utf-8')
-    #         parent_cost_center = row[6].decode('utf-8')
-    #         parent_cost_center = frappe.get_value(
-    #             "Cost Center",
-    #             dict(
-    #                 cost_center_name=parent_cost_center,
-    #                 company=company.name,
-    #             ),
-    #             "name"
-    #         )
-    #         children_units = row[4]
-    #         doc = frappe.get_doc(
-    #             dict(
-    #                 doctype="Cost Center",
-    #                 cost_center_name=cost_center_name,
-    #                 parent_cost_center=parent_cost_center,
-    #                 company=company.name,
-    #                 is_group=int(children_units) > 0
-    #             )
-    #         )
-    #         doc.flags.ignore_mandatory = True
-    #         try:
-    #
-    #             doc.insert(ignore_permissions=True)
-    #         except frappe.exceptions.DuplicateEntryError:
-    #             pass
-    #         print serial_no
-    # frappe.db.commit()
+            journal_entry.flags.ignore_permissions = True
 
-    # # print("Done Cost Centers")
-    # print ("****************")
-    # print("Starting Accounts..")
-    #
-    # current_file = get_file_path(accounts_tree)
-    # # current_file = current_file.replace("s1.essal.co", "alnamaa.s1.essal.co")
-    #
-    # with open(current_file, 'rb') as csvfile:
-    #     spamreader = csv.reader(csvfile, delimiter=str(","), quotechar=str("|"))
-    #     for row in spamreader:
-    #         try:
-    #             serial_no = int(row[0])
-    #         except:
-    #             continue
-    #         # print row
-    #         account_name = row[1].decode('utf-8')
-    #         account_type = row[2].decode('utf-8')
-    #         children_units = row[8]
-    #         parent_account = row[10].decode('utf-8') if row[10] else None
-    #
-    #         if parent_account:
-    #             parent_acc = frappe.get_value(
-    #                 "Account",
-    #                 dict(
-    #                     account_name=("like", "%{0}".format(parent_account)),
-    #                     company=company.name
-    #                 ),
-    #                 "name"
-    #             )
-    #         else:
-    #             parent_acc = None
-    #         doc = frappe.get_doc(
-    #             dict(
-    #                 doctype="Account",
-    #                 account_name=account_name,
-    #                 account_number=None,
-    #                 account_serial=serial_no,
-    #                 company=company.name,
-    #                 is_group=float(children_units.replace(" ", "")) > 0,
-    #                 # account_currency="SAR",
-    #                 parent_account=parent_acc,
-    #                 report_type="Balance Sheet",
-    #                 root_type="Expense"
-    #             )
-    #         )
-    #         doc.flags.ignore_mandatory = True
-    #         try:
-    #             doc.insert(ignore_permissions=True)
-    #         except frappe.exceptions.DuplicateEntryError:
-    #             if frappe.db.exists(
-    #                     "Account",
-    #                     dict(
-    #                         company=company.name,
-    #                         account_serial=serial_no
-    #                     )
-    #             ):
-    #                 continue
-    #             else:
-    #                 account_name = "{0} - {1}".format(parent_account, account_name)
-    #                 doc.name = None
-    #                 doc.account_name = account_name
-    #                 doc.insert(ignore_permissions=True)
-    #
-    #         except frappe.exceptions.ValidationError:
-    #             frappe.db.set_value(
-    #                 "Account",
-    #                 parent_acc,
-    #                 "is_group",
-    #                 1
-    #             )
-    #             doc.insert(ignore_permissions=True)
-    #         frappe.db.set_value(
-    #             "Account",
-    #             doc.name,
-    #             "account_serial",
-    #             serial_no
-    #         )
-    #     print serial_no
-    # print("Done Accounts.")
-    # frappe.db.commit()
-    # print ("****************")
-    print("Starting Journal Entries..")
-    current_file = get_file_path(journal_file)
-    # current_file = current_file.replace("s1.essal.co", "alnamaa.s1.essal.co")
+            journal_entry.submit()
+
+            print journal_entry.name
+
+    print("Done payment..")
+
+    print("Start Payments details..")
+    current_file = get_file_path(payment_details)
 
     with open(current_file, 'rb') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=str(","), quotechar=str("|"))
-        current_entry_index, total_credit, total_debit = 0, 0, 0
-        journal_entry = None
         for row in spamreader:
             # print row
             try:
@@ -285,111 +189,107 @@ def execute():
                 continue
             if not serial_no:
                 continue
-            journal_date = str(row[1].decode('utf-8')).split("/")
-            day = int(journal_date[2].replace(" ", ""))
-            month = int(journal_date[1].replace(" ", ""))
-            year = int(journal_date[0].replace(" ", ""))
-            journal_date = HijriDate(year, month, day, gr=False)
-            journal_date = "{:04d}-{:02d}-{:02d}".format(int(journal_date.year_gr), int(journal_date.month_gr),
-                                                         int(journal_date.day_gr))
-            print journal_date
-            if serial_no != current_entry_index:
-                if journal_entry:
-                    journal_entry.total_debit = abs(total_debit)
-                    journal_entry.total_credit = abs(total_credit)
-                    journal_entry.difference = abs(total_debit - total_credit)
-                    journal_entry.insert(ignore_permissions=True)
-                    journal_entry.flags.ignore_permissions = True
+            serial_no = "{:05d}".format(serial_no)
+            post_date = row[1]
+            post_date = datetime.strptime(
+                str(post_date), '%Y/%m/%d'
+            )
+            total = float(row[2])
 
-                    journal_entry.submit()
+            account_no = row[3]
+            pay_no = row[4]
+            serialno = row[5]
+            remark = row[6]
+            moa_no_name = row[7]
+            pay_name = row[8]
+            akar_no = row[9]
+            akar_address = row[10]
 
-                    print journal_entry.name
+            remark_str = ""
+            if pay_no:
+                remark_str = remark_str + "PAY_NO: {0}".format(pay_no)
+            if serialno:
+                if remark_str:
+                    remark_str += ", "
+                remark_str = remark_str + "SerialNo: {0}".format(serialno)
+            if remark:
+                if remark_str:
+                    remark_str += ", "
+                remark_str = remark_str + "REMARK: {0}".format(remark)
+            if moa_no_name:
+                if remark_str:
+                    remark_str += ", "
+                remark_str = remark_str + "MoaNoName: {0}".format(moa_no_name)
+            if pay_name:
+                if remark_str:
+                    remark_str += ", "
+                remark_str = remark_str + "PAY_NAME: {0}".format(pay_name)
+            if akar_no:
+                if remark_str:
+                    remark_str += ", "
+                remark_str = remark_str + "AKAR_NO: {0}".format(akar_no)
+            if akar_address:
+                if remark_str:
+                    remark_str += ", "
+                remark_str = remark_str + "AKAR_ADDRESS: {0}".format(akar_address)
 
-                journal_entry = frappe.get_doc(
-                    dict(
-                        doctype="Journal Entry",
-                        title="{1} القيد رقم ({0}) سنة".format(serial_no, year),
-                        voucher_type="Journal Entry",
-                        naming_series="JV-",
-                        posting_date=journal_date,
-                        company=company.name,
-                        user_remark="{1} القيد رقم ({0}) سنة".format(serial_no, year),
-                        multi_currency=0,
-                        remark="{1} القيد رقم ({0}) سنة".format(serial_no, year),
-                        bill_date=datetime.now(),
-                        third_party_creation=journal_date,
-                        accounts=[],
-                        is_opening="Yes" if serial_no == 1 else "No"
-                    )
+            account_name = frappe.get_value(
+                "Account",
+                dict(
+                    parent=parent_account,
+                    name=("like", "%- {0} -%".format(account_no))
                 )
-                current_entry_index = serial_no
+            )
+            journal_entry = frappe.get_doc(
+                dict(
+                    doctype="Journal Entry",
+                    title=str(serial_no),
+                    voucher_type="Journal Entry",
+                    naming_series="JV-",
+                    posting_date=post_date,
+                    company=company.name,
+                    user_remark=remark,
+                    multi_currency=0,
+                    remark=remark_str,
+                    bill_date=datetime.now(),
+                    third_party_creation=post_date,
+                    accounts=[],
+                    is_opening="No"
+                )
+            )
 
-            account_name = row[2].decode('utf-8')
-            account_name = account_name[account_name.index("[") + 1:account_name.index("]")]
-            print account_name
-            account_name = frappe.get_value("Account",
-                                            dict(
-                                                company=company.name,
-                                                account_serial=account_name
-                                            ),
-                                            "name")
-            if not account_name:
-                print("no account")
-            # print account_name
-            debit = float(row[3])
-            credit = float(row[4])
-            description = row[7].decode('utf-8')
-
-            if credit:
-                total_credit += credit
-            else:
-                total_debit += debit
             journal_entry.append("accounts", dict(
-                account=account_name,
+                account=cash_account,
+                against_account=account_name,
                 party_type=None,
                 party=None,
-                title=description,
+                title=remark,
                 exchange_rate=1,
-                debit_in_account_currency=abs(debit),
-                debit=abs(debit),
-                journal_note=description,
-                credit_in_account_currency=abs(credit),
-                credit=abs(credit),
+                debit_in_account_currency=abs(total),
+                debit=abs(total),
+                journal_note=remark,
+                credit_in_account_currency=abs(0),
+                credit=abs(0),
                 is_advance="No",
                 cost_center=main_cost_center
             ))
-            frappe.db.commit()
-    if journal_entry:
-        journal_entry.total_debit = abs(total_debit)
-        journal_entry.total_credit = abs(total_credit)
-        journal_entry.difference = abs(total_debit - total_credit)
-        journal_entry.insert(ignore_permissions=True)
-        journal_entry.flags.ignore_permissions = True
+            journal_entry.append("accounts", dict(
+                account=account_name,
+                party_type="Company",
+                party=company.name,
+                title=remark,
+                exchange_rate=1,
+                debit_in_account_currency=0,
+                debit=0,
+                journal_note=remark,
+                credit_in_account_currency=abs(total),
+                credit=abs(total),
+                is_advance="No",
+                cost_center=main_cost_center
+            ))
+            journal_entry.flags.ignore_permissions = True
 
-        journal_entry.submit()
+            journal_entry.submit()
 
-        print journal_entry.name
-    print("Done Journals.")
-
-    frappe.db.sql(
-        """UPDATE `tabJournal Entry` SET `creation` = `posting_date`, `modified` = `posting_date`
-WHERE `company` = %s;""", (company.name,))
-
-    journals = frappe.get_list(
-        "Journal Entry",
-        filters=dict(
-            company="Alnama1439",
-            docstatus=0
-        ),
-        ignore_permissions=True,
-        ignore_ifnull=True
-    )
-
-    for journal in journals:
-        doc = frappe.get_doc("Journal Entry", journal.name)
-        doc.flags.ignore_permissions = True
-
-        doc.submit()
-        frappe.db.commit()
-        print doc.name
-
+            print journal_entry.name
+    print("Done payment details..")
