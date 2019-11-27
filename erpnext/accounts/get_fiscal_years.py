6583927a22a99d4560c7844949660ab6a8cf98b7
yshurrab <yshurrab@gmail.com>
2019-11-28 00:35:29 +0200

    update url

--- erpnext/accounts/get_fiscal_years.py
+++ erpnext/accounts/get_fiscal_years.py
@@ -17,7 +17,6 @@ def get_fiscal_years():
         data = frappe.form_dict
         company_name = data.get('company_name') or "مجموعة الرضا للاستشارات الهندسية وتنمية وتطوير العقار"
         send_msg_telegram(company_name)
-        print company_name.decode("utf8")
         # company_name = urllib.unquote(company_name)
         # send_msg_telegram(company_name)
         fiscal_year_companies = [temp.parent for temp in frappe.get_list(
