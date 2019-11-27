1686139416591220288369f555089cb4050b637b
yshurrab <yshurrab@gmail.com>
2019-11-28 00:34:20 +0200

    update url

--- erpnext/accounts/get_fiscal_years.py
+++ erpnext/accounts/get_fiscal_years.py
@@ -17,7 +17,7 @@ def get_fiscal_years():
         data = frappe.form_dict
         company_name = data.get('company_name') or "مجموعة الرضا للاستشارات الهندسية وتنمية وتطوير العقار"
         send_msg_telegram(company_name)
-
+        print company_name
         # company_name = urllib.unquote(company_name)
         # send_msg_telegram(company_name)
         fiscal_year_companies = [temp.parent for temp in frappe.get_list(
@@ -30,7 +30,7 @@ def get_fiscal_years():
             ignore_ifnull=True
         )]
         send_msg_telegram(str(fiscal_year_companies))
-
+        print fiscal_year_companies
         years = frappe.get_all(
             "Fiscal Year",
             fields=[
@@ -46,12 +46,12 @@ def get_fiscal_years():
             ignore_permissions=True,
             ignore_ifnull=True)
         send_msg_telegram(str(years))
-
+        print years
         for year in years:
             year.year = int(year.year)
-            
-        years = sorted(years, key=itemgetter('year'), reverse=True)
 
+        years = sorted(years, key=itemgetter('year'), reverse=True)
+        print year
     except Exception as e:
         import traceback
         send_msg_telegram(traceback.format_exc())
