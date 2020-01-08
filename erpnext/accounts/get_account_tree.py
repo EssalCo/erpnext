0ab61a50b85ec3b1198fb159100a1d7e2f749a9e
yshurrab <yshurrab@gmail.com>
2020-01-08 23:39:31 +0200

    fix

--- erpnext/accounts/get_account_tree.py
+++ erpnext/accounts/get_account_tree.py
@@ -23,7 +23,7 @@ def get_account_tree():
         data = frappe.form_dict.data
         send_msg_telegram(str(data) + str(type(data)))
         _data = frappe.form_dict
-        send_msg_telegram(_data + str(type(_data)))
+        send_msg_telegram(str(_data) + str(type(_data)))
         company_name = data['company_name']
         if company_name and '%' in company_name:
             company_name = urllib.unquote(str(data['company_name'])).decode('utf-8', 'replace')
