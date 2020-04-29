cc38863674f13b784c6362172d19b202748069d7
yshurrab <yshurrab@gmail.com>
2020-04-29 02:41:34 +0200

    update

---
+++ erpnext/cron_jobs/migrate_journal_entries_ids.py
@@ -0,0 +1,307 @@
+import frappe
+
+
+def execute():
+
+    company_name = "asaas"
+    prefix = frappe.get_value("Company", company_name, "series_prefix")
+    if not prefix: return
+    journals = frappe.db.sql(
+        """SELECT name
+        FROM `tabJournal Entry`
+        WHERE company = %(company)s
+        ORDER by creation ASC;""", as_dict=True
+    )
+
+    for journal in journals:
+        old_name = journal.name
+        new_name = _make_autoname(key='{prefix}.#######'.format(prefix=prefix))
+        print old_name,",",new_name
+        frappe.db.set_value(
+            "Journal Entry",
+            old_name,
+            "name",
+            new_name,
+            update_modified=False
+        )
+
+        frappe.db.set_value(
+            "Journal Entry Account",
+            dict(
+                reference_name=old_name
+            ),
+            "reference_name",
+            new_name,
+            update_modified=False
+        )
+        frappe.db.set_value(
+            "Journal Entry Account",
+            dict(
+                parent=old_name
+            ),
+            "parent",
+            new_name,
+            update_modified=False
+        )
+
+
+        frappe.db.set_value(
+            "Payment Reconciliation Invoice",
+            dict(
+                invoice_number=old_name
+            ),
+            "invoice_number",
+            new_name,
+            update_modified=False
+        )
+
+        frappe.db.set_value(
+            "Depreciation Schedule",
+            dict(
+                journal_entry=old_name
+            ),
+            "journal_entry",
+            new_name,
+            update_modified=False
+        )
+
+
+        frappe.db.set_value(
+            "Salary Slip",
+            dict(
+                journal_entry=old_name
+            ),
+            "journal_entry",
+            new_name,
+            update_modified=False
+        )
+
+        frappe.db.set_value(
+            "Journal Entry",
+            dict(
+                amended_from=old_name
+            ),
+            "amended_from",
+            new_name,
+            update_modified=False
+        )
+        frappe.db.set_value(
+            "Stock Entry",
+            dict(
+                credit_note=old_name
+            ),
+            "credit_note",
+            new_name,
+            update_modified=False
+        )
+
+        frappe.db.set_value(
+            "GL Entry",
+            dict(
+                against_voucher=old_name
+            ),
+            "against_voucher",
+            new_name,
+            update_modified=False
+        )
+        frappe.db.set_value(
+            "GL Entry",
+            dict(
+                voucher_no=old_name
+            ),
+            "voucher_no",
+            new_name,
+            update_modified=False
+        )
+
+        frappe.db.set_value(
+            "Bank Reconciliation Detail",
+            dict(
+                payment_entry=old_name
+            ),
+            "payment_entry",
+            new_name,
+            update_modified=False
+        )
+
+        frappe.db.set_value(
+            "Stock Ledger Entry",
+            dict(
+                voucher_no=old_name
+            ),
+            "voucher_no",
+            new_name,
+            update_modified=False
+        )
+
+
+        frappe.db.set_value(
+            "Purchase Invoice Advance",
+            dict(
+                reference_name=old_name
+            ),
+            "reference_name",
+            new_name,
+            update_modified=False
+        )
+        frappe.db.set_value(
+            "Sales Invoice Advance",
+            dict(
+                reference_name=old_name
+            ),
+            "reference_name",
+            new_name,
+            update_modified=False
+        )
+
+        frappe.db.set_value(
+            "Payment Entry Reference",
+            dict(
+                reference_name=old_name
+            ),
+            "reference_name",
+            new_name,
+            update_modified=False
+        )
+
+        frappe.db.set_value(
+            "Payment Reconciliation",
+            dict(
+                reference_name=old_name
+            ),
+            "reference_name",
+            new_name,
+            update_modified=False
+        )
+
+
+        frappe.db.set_value(
+            "Payment Request",
+            dict(
+                reference_name=old_name
+            ),
+            "reference_name",
+            new_name,
+            update_modified=False
+        )
+
+        frappe.db.set_value(
+            "Subscription",
+            dict(
+                reference_document=old_name
+            ),
+            "reference_document",
+            new_name,
+            update_modified=False
+        )
+        frappe.db.set_value(
+            "Patient Medical Record",
+            dict(
+                reference_name=old_name
+            ),
+            "reference_name",
+            new_name,
+            update_modified=False
+        )
+
+        frappe.db.set_value(
+            "Maintenance Visit Purpose",
+            dict(
+                prevdoc_docname=old_name
+            ),
+            "prevdoc_docname",
+            new_name,
+            update_modified=False
+        )
+
+
+        frappe.db.set_value(
+            "Authorization Rule",
+            dict(
+                reference_name=old_name
+            ),
+            "reference_name",
+            new_name,
+            update_modified=False
+        )
+
+        frappe.db.set_value(
+            "Landed Cost Item",
+            dict(
+                receipt_document=old_name
+            ),
+            "receipt_document",
+            new_name,
+            update_modified=False
+        )
+
+
+        frappe.db.set_value(
+            "Landed Cost Purchase Receipt",
+            dict(
+                receipt_document=old_name
+            ),
+            "receipt_document",
+            new_name,
+            update_modified=False
+        )
+
+        frappe.db.set_value(
+            "Quality Inspection",
+            dict(
+                reference_name=old_name
+            ),
+            "reference_name",
+            new_name,
+            update_modified=False
+        )
+
+        frappe.db.set_value(
+            "Serial No",
+            dict(
+                purchase_document_no=old_name
+            ),
+            "purchase_document_no",
+            new_name,
+            update_modified=False
+        )
+        frappe.db.set_value(
+            "Serial No",
+            dict(
+                delivery_document_no=old_name
+            ),
+            "delivery_document_no",
+            new_name,
+            update_modified=False
+        )
+
+        frappe.db.set_value(
+            "Stock Ledger Entry",
+            dict(
+                voucher_no=old_name
+            ),
+            "voucher_no",
+            new_name,
+            update_modified=False
+        )
+
+
+def _make_autoname(key='Agent.-.#####', year=None):
+    from frappe.model.naming import getseries
+    from frappe.utils import now_datetime
+    parts = key.split('.')
+    n = ''
+    for e in parts:
+        if e.startswith('#'):
+            digits = len(e)
+            part = getseries(n, digits, "")
+        elif e == 'YYYY':
+            if year:
+                part = str(year)
+            else:
+                part = now_datetime().strftime('%Y')
+        else:
+            part = e
+
+        n += part
+    return n
