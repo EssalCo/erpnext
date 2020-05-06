a52bc8803d6722d56c33c059a7e2709f38e59a5e
yshurrab <yshurrab@gmail.com>
2020-05-06 17:33:18 +0200

    add two aPIs

---
+++ erpnext/accounts/get_activity_log.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from datetime import datetime
+import erpnext
+from frappe import _
+import frappe
+
+
+def get_activity_log():
+    # 'account',
+    try:
+        data = frappe.form_dict
+
+        user_id = data.get('user_id')
+        from_date = data.get('from_date')
+        to_date = data.get('to_date')
+        frappe.set_user("Administrator")
+
+        logs = frappe.db.sql("""SELECT `NAME`,
+	`creation`,
+	`full_name`,
+	`operation`,
+	`SUBJECT`,
+	`STATUS`,
+    `USER` 
+FROM
+	`tabActivity Log`
+	WHERE 
+	`creation` BETWEEN %(from_date)s AND %(to_date)s
+	{user_condition};""".format(
+            user_condition=" AND `user` = '{0}'".format(user_id) if user_id else ""
+        ), dict(from_date=from_date, to_date=to_date), as_dict=True)
+
+    except Exception as e:
+        return dict(status=False, message=str(e))
+    return dict(status=True, message="Success", logs=logs)
---
+++ erpnext/accounts/get_users.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import frappe
+
+
+def get_users():
+    try:
+        users = frappe.db.sql("""SELECT
+	u.`NAME` 
+FROM
+	`tabUser` u 
+WHERE
+	u.`enabled` = 1 
+	AND u.`user_type` = 'System User';""", as_dict=True)
+    except Exception as e:
+        return dict(status=False, message=str(e))
+    return dict(status=True, message="Success", users=users)
