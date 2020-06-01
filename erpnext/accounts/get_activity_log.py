70c39ae75da55d260b1ada34389c21929dd86491
yshurrab <yshurrab@gmail.com>
2020-06-01 12:14:27 +0200

    transalte logs

--- erpnext/accounts/get_activity_log.py
+++ erpnext/accounts/get_activity_log.py
@@ -33,6 +33,14 @@ FROM
             user_condition=" AND `user` = '{0}'".format(user_id) if user_id else ""
         ), dict(from_date=from_date, to_date=to_date), as_dict=True)
 
+        for log in logs:
+            log.full_name = _(log.full_name, "ar")
+            log.operation = _(log.operation, "ar")
+
+            log.subject = _(log.subject, "ar")
+            log.status = _(log.status, "ar")
+            log.user = _(log.user, "ar")
+
     except Exception as e:
         return dict(status=False, message=str(e))
     return dict(status=True, message="Success", logs=logs)
