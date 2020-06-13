f832472f415464626dedf5cbcb25e8ee678db6af
yshurrab <yshurrab@gmail.com>
2020-06-13 23:24:26 +0200

    add delete script

---
+++ erpnext/cron_jobs/delete_entries.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+import  frappe
+
+
+def execute():
+
+    company = "أعمال النماء"
+    frappe.db.sql("""delete from `tabGL Entry` where voucher_type='Journal Entry' and company = '{0}';""".format(
+        company
+    ))
+
+    frappe.db.sql("""delete from `tabJournal Entry` where company = '{0}';""".format(
+        company
+    ))
+
+    frappe.db.sql("""delete from `tabSeries` where name = 'ANAM';""")
