e67b2665d1a0eff667f972b38883dddaf91b3fc5
yshurrab <yshurrab@gmail.com>
2020-06-30 21:09:41 +0200

    update

--- erpnext/accounts/doctype/journal_entry/journal_entry.js
+++ erpnext/accounts/doctype/journal_entry/journal_entry.js
@@ -657,8 +657,8 @@ function filterCustomer(frm) {
 	cur_frm.fields_dict['accounts'].grid.get_field('party').get_query = function (doc) {
 
 		/* Get parties of this customer group only if filled */
-		console.log(cur_frm.fields_dict);
-		console.log(cur_dialog.fields_dict);
+		console.log(cur_frm);
+		// console.log(cur_dialog.fields_dict);
 
 		return {
 			query: 'erpnext.account.doctype.filter_customer_group.filter_customer_group',
