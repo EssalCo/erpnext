a333a3262aa0961c78d65a15ad7fc39d30ac22ad
yshurrab <yshurrab@gmail.com>
2020-07-06 03:40:02 +0200

    update

--- erpnext/controllers/queries.py
+++ erpnext/controllers/queries.py
@@ -329,8 +329,8 @@ def get_customer_list(doctype, txt, searchfield, start, page_len, filters):
 	is_customer = False
 
 	for d in filters:
-		if d[3] and d[0] == "Customer" and d[1] == "customer_group":
-			if frappe.get_value(
+		if d[0] == "Customer" and d[1] == "customer_group":
+			if d[3] and frappe.get_value(
 			"Customer Group",
 			d[3],
 			"parent_customer_group"
