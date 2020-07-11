946cf90430b5d06b1ea9d9ca76430b596b8dd80b
yshurrab <yshurrab@gmail.com>
2020-07-12 01:29:44 +0200

    update gl note

---
+++ erpnext/cron_jobs/update_gl_note.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import sys
+
+import frappe
+
+sys.path.append('../..')
+import csv
+from frappe.utils.file_manager import get_file_path
+
+
+def execute():
+
+    _file = "/private/files/nawat_ledger_notes.csv"
+    current_file = get_file_path(_file)
+
+    with open(current_file, 'rb') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=str(","), quotechar=str("|"))
+        for row in spamreader:
+            try:
+                gl_id = int(row[0])
+            except:
+                continue
+
+            print gl_id
+            note = row[1]
+
+            frappe.db.sql(
+                """UPDATE `tabGL Entry` SET journal_note = %(journal_note)s WHERE name = %(name)s AND (journal_note IS NULL OR journal_note = "");""", dict(
+                    journal_note=note,
+                    name=gl_id
+                )
+            )
