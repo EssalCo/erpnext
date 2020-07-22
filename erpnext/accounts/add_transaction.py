eb810c15e27cbb515e7af4d673d8d7cdc7bea5fb
yshurrab <yshurrab@gmail.com>
2020-07-22 11:06:40 +0200

    update

--- erpnext/accounts/add_transaction.py
+++ erpnext/accounts/add_transaction.py
@@ -812,6 +812,7 @@ def add_transaction_v3():
         # data = x
         data = frappe.form_dict.get('data')
         send_msg_telegram(site_name)
+        send_msg_telegram(data)
         if isinstance(data, basestring):
             import json
             data = json.loads(data)
