5d5f35f4e9c15850e2657b0f2749bf58edc037cd
yshurrab <yshurrab@gmail.com>
2020-08-04 22:32:32 +0200

    Remove project

--- erpnext/cron_jobs/fix_project_in_alnamaa.py
+++ erpnext/cron_jobs/fix_project_in_alnamaa.py
@@ -7,7 +7,7 @@ def execute():
     data = frappe.db.sql(
         """select left(j.title,7), ja.project, count(*)  from `tabJournal Entry Account` ja 
 inner join `tabJournal Entry` j on j.name = ja.parent group by ja.project, left(j.title, 7) 
- having ja.project is not null and left(j.title,7) = "أساس - "
+ having ja.project is not null and left(ja.title,7) = "أساس - "
 order by left(j.title,1);""", as_dict=True
     )
 
