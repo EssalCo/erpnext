39e64d61e8a67883d0e448ec0424e462c5230622
yshurrab <yshurrab@gmail.com>
2020-10-29 17:34:48 +0200

    update API

--- erpnext/accounts/create_task.py
+++ erpnext/accounts/create_task.py
@@ -14,6 +14,8 @@ def create_task():
         description = data.get('description')
         expected_start_date = data.get('expected_start_date')
         project = data.get('project')
+        assigned_to = data.get('assigned_to')
+        administrator_email = data.get('administrator_email')
 
         task = frappe.get_doc({
             "doctype": "Task",
@@ -26,6 +28,17 @@ def create_task():
         task.flags.ignore_mandatory = True
         task.insert(ignore_permissions=True)
 
+        # create a todo
+        frappe.get_doc({
+            "doctype": "ToDo",
+            "owner": assigned_to,
+            "assigned_by": administrator_email,
+            "description": task.description,
+            "reference_type": "Task",
+            "reference_name": task.name
+        }).insert(ignore_permissions=True)
+
+        frappe.db.commit()
     except Exception as e:
         return dict(status=False, message=str(e))
     return dict(status=True, message="Success", )
