1cbce5f9587a58bf57a138d1c046ba12c8113207
yshurrab <yshurrab@gmail.com>
2020-11-03 23:01:53 +0200

    update script

---
+++ erpnext/cron_jobs/copy_project_from_cost_center.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+
+import  frappe
+
+
+def execute():
+    company = "أعمال النماء"
+    entries = frappe.db.sql("""SELECT name, cost_center, project from `tabGL Entry` where voucher_type='Journal Entry' and company = '{0}'
+     and project is null and cost_center is not null;""".format(
+        company
+    ))
+
+    for entry in entries:
+        print entry.name
+        print entry.cost_center
+
+        cost_center = frappe.get_value(
+            "Cost Center",
+            dict(
+                name=entry.cost_center
+            ), "cost_center_name"
+        )
+        print cost_center
+        if cost_center:
+            project = frappe.get_value(
+                "Project",
+                dict(
+                    project_name=cost_center
+                )
+            )
+            print str(project)
+            if project:
+                # frappe.db.sql(
+                #     """update `tabGL Entry` SET project = '{0}' WHERE name = '{1}';""".format(
+                #         project,
+                #         entry.name
+                #     )
+                # )
+                pass
+            else:
+                print("ERROR: no project found!!")
+                continue
+        else:
+            print("ERROR: no cost center!!")
+            continue
+
+    entries = frappe.db.sql("""SELECT name, cost_center from `tabJournal Entry Account` where voucher_type='Journal Entry' and company = '{0}' 
+    and project is null and cost_center is not null;""".format(
+        company
+    ))
+    print entries
