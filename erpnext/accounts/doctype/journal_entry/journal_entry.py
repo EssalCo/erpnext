96abcbe78696be168503c32a63ad9270c50db8ca
yshurrab <yshurrab@gmail.com>
2020-11-10 10:21:36 +0200

    add do not merge

--- erpnext/accounts/doctype/journal_entry/journal_entry.py
+++ erpnext/accounts/doctype/journal_entry/journal_entry.py
@@ -112,7 +112,7 @@ class JournalEntry(AccountsController):
         except:
             site_name = ""
         do_not_merge_similar_entries = not bool(getattr(self, "do_not_merge_similar_entries", False))
-        if site_name in ("osaan.s1.essal.co", "alnamaa.s1.essal.co"):
+        if site_name in ("osaan.s1.essal.co", "alnamaa.s1.essal.co", "tahlia.s1.essal.co"):
             self.do_not_merge_similar_entries = True
         if not self.owner_name:
             self.owner_name = frappe.get_value("User", self.owner, "full_name")
