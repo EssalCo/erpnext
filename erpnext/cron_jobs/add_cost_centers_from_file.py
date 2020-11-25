ee6368d293ab5f6facd3669598f297390c9eff95
yshurrab <yshurrab@gmail.com>
2020-11-25 20:24:05 +0200

    script for cost centers

--- erpnext/cron_jobs/add_cost_centers_from_file.py
+++ erpnext/cron_jobs/add_cost_centers_from_file.py
@@ -70,7 +70,9 @@ def execute():
             second_row = row[1].decode('utf-8').replace(" ", "")
             print row[0]
             print row[1]
-            if not first_row and not second_row: continue
+            if not first_row and not second_row:
+                print("XXXXX")
+                continue
             if not first_row:
                 parent_cost_center = row[1].decode('utf-8')
                 doc = frappe.get_doc(
