24e13e6ea242ad1594442d6f9ab38e18a7067464
yshurrab <yshurrab@gmail.com>
2020-11-25 19:44:36 +0200

    script for cost centers

--- erpnext/cron_jobs/add_cost_centers_from_file.py
+++ erpnext/cron_jobs/add_cost_centers_from_file.py
@@ -67,6 +67,7 @@ def execute():
             print(counter)
             # print row
             first_row = row[0]
+            print(first_row)
             if not first_row:
                 parent_cost_center = row[1]
                 doc = frappe.get_doc(
