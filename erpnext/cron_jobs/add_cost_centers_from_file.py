3577c345b014eb172291f55ca2ac0b15f8a8a0ab
yshurrab <yshurrab@gmail.com>
2020-11-25 19:42:09 +0200

    script for cost centers

--- erpnext/cron_jobs/add_cost_centers_from_file.py
+++ erpnext/cron_jobs/add_cost_centers_from_file.py
@@ -21,35 +21,35 @@ def execute():
     )
     print(companies)
     company = companies[0]
-    if frappe.db.count(
-            "Cost Center",
-            dict(
-                company=company.name
-            )
-    ) != 0:
-        doc = frappe.get_doc(
-            "Cost Center",
-            dict(
-                parent_cost_center=None,
-                company=company.name,
-            )
-        )
-    else:
-        doc = frappe.get_doc(
-            dict(
-                doctype="Cost Center",
-                cost_center_name=company.name,
-                parent_cost_center=None,
-                company=company.name,
-                is_group=1
-            )
-        )
-        doc.flags.ignore_mandatory = True
-        try:
-            doc.insert(ignore_permissions=True)
-        except frappe.exceptions.DuplicateEntryError as e:
-            print(e)
-
+    # if frappe.db.count(
+    #         "Cost Center",
+    #         dict(
+    #             company=company.name
+    #         )
+    # ) != 0:
+    #     doc = frappe.get_doc(
+    #         "Cost Center",
+    #         dict(
+    #             parent_cost_center=None,
+    #             company=company.name,
+    #         )
+    #     )
+    # else:
+    #     doc = frappe.get_doc(
+    #         dict(
+    #             doctype="Cost Center",
+    #             cost_center_name=company.name,
+    #             parent_cost_center=None,
+    #             company=company.name,
+    #             is_group=1
+    #         )
+    #     )
+    #     doc.flags.ignore_mandatory = True
+    #     try:
+    #         doc.insert(ignore_permissions=True)
+    #     except frappe.exceptions.DuplicateEntryError as e:
+    #         print(e)
+    main_cost_center = 'رئيسي - م'
     file = "/private/files/mrfq_costs_centers.csv"
     current_file = get_file_path(file)
 
@@ -72,7 +72,7 @@ def execute():
                     dict(
                         doctype="Cost Center",
                         cost_center_name=parent_cost_center,
-                        parent_cost_center=doc.name,
+                        parent_cost_center=main_cost_center,
                         company=company.name,
                         is_group=1
                     )
