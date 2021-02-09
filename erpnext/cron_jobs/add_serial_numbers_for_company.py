a998c1886d249afb76168b2e30939d573ee49714
yshurrab <yshurrab@gmail.com>
2021-02-09 13:34:28 +0200

    add company

--- erpnext/cron_jobs/add_serial_numbers_for_company.py
+++ erpnext/cron_jobs/add_serial_numbers_for_company.py
@@ -29,18 +29,20 @@ def execute():
         for acc in accounts:
             account = frappe.get_doc("Account", acc.name)
             last_existing_serial = frappe.db.sql("""SELECT 
-                        MAX(account_serial * 1) AS maxi
-                    FROM
-                        tabAccount
-                    WHERE parent_account IS NULL;""", as_dict=True)
+        MAX(account_serial) AS maxi
+    FROM
+        tabAccount
+    WHERE 
+        company = %s
+            AND parent_account IS NULL;""", (company,), as_dict=True)
             if len(last_existing_serial) == 0 or not last_existing_serial[0].maxi:
                 # last_existing_serial = 0
                 next_serial = 1
                 next_serial_str = "#1"
             else:
-                last_existing_serial = last_existing_serial[0].maxi or 0
-                next_serial = last_existing_serial + 1
-                next_serial_str = "#{0}".format(last_existing_serial + 1)
+                last_existing_serial = last_existing_serial[0].maxi
+                next_serial = int(last_existing_serial or 0) + 1
+                next_serial_str = "#{0}".format(next_serial)
             frappe.db.sql("""UPDATE `tabAccount` SET `account_serial` = '{0}',
                          `account_serial_x` = '{1}' WHERE `name` = '{2}';""".format(
                 int(next_serial),
@@ -64,7 +66,7 @@ def execute():
         for account in accounts:
             serial = account.account_name.split(" -")[0]
             if serial.isdigit():
-                account.account_name = account.account_name.split(" -")[0].replace(account.account_name.split(" -")[0], "")
+                account.account_name = account.account_name.replace(account.account_name.split(" -")[0], "")[2:]
             frappe.db.set_value(
                 "Account",
                 account.name,
@@ -91,52 +93,52 @@ def update_children_serials(parent_account):
         # send_msg_telegram("parent " + str(self.account_serial) + str(self.account_serial_x))
 
         last_existing_serial = frappe.db.sql("""SELECT account_serial, account_serial_x, name FROM
-          `tabAccount` WHERE
-           account_serial = (
-        SELECT 
-            MAX(account_serial * 1) AS maxi
-        FROM
-            tabAccount
-        WHERE 
-             parent_account = %s
-                )
-                ORDER BY `creation` ASC LIMIT 1;""", (account.parent_account,), as_dict=True)
-        parent_serial_x, parent_serial = frappe.db.get_value(
+      `tabAccount` WHERE
+       account_serial = (
+    SELECT 
+        MAX(account_serial * 1) AS maxi
+    FROM
+        tabAccount
+    WHERE 
+        company = %s
+            AND parent_account = %s);""", (account.company, account.parent_account), as_dict=True)
+        parent_serial, account_serial_x = frappe.db.get_value(
             "Account",
             account.parent_account,
             [
-                "account_serial_x",
-                "account_serial"
+                "account_serial",
+                "account_serial_x"
             ]
         )
+        # send_msg_telegram("parent acc " + str(parent_serial) + " " + str(account_serial_x))
+        # send_msg_telegram("parent account " + str(last_existing_serial))
+
         if len(last_existing_serial) == 0 or not last_existing_serial[0].account_serial:
 
-            print "Child"
-            print parent_serial
-            print parent_serial_x
-            last_existing_serial = int(parent_serial) * 100
+            last_existing_serial = long(parent_serial) * 100
+            # send_msg_telegram("sum " + str(last_existing_serial))
             next_serial = last_existing_serial + 1
+            next_serial_str = "{0}.{1}".format(parent_serial, 1)
         else:
-            _last_existing_serial = int(last_existing_serial[0].account_serial)
-            next_serial = _last_existing_serial + 1
-        next_serial_str = "{0}.{1}".format(parent_serial_x, next_serial)
+
+            last_existing_serial = long(last_existing_serial[0].account_serial)
+            # send_msg_telegram("query " + str(last_existing_serial))
+
+            next_serial = last_existing_serial + 1
+
+            # trimmed_serial = str(last_existing_serial[0].account_serial_x).split(".")[-1]
+            next_serial_str = "{0}.{1}".format(account_serial_x, next_serial)
 
             # trimmed_serial = str(last_existing_serial[0].account_serial_x).split(".")[-1]
             # next_serial_str = "{0}.{1}".format(parent_serial, int(trimmed_serial) + 1)
         # send_msg_telegram("finish " + str(self.account_serial) + str(self.account_serial_x))
-        print str(next_serial_str)
-        print str(next_serial)
+        print(str(next_serial_str))
+        print(str(next_serial))
         frappe.db.sql("""UPDATE `tabAccount` SET `account_serial` = '{0}', 
         `account_serial_x` = '{1}' WHERE `name` = '{2}';""".format(
             int(next_serial),
             next_serial_str,
             account.name
-        ))
-        print("""UPDATE `tabAccount` SET `account_serial` = '{0}',
-                 `account_serial_x` = '{1}' WHERE `name` = '{2}';""".format(
-            next_serial,
-            next_serial_str,
-            "account name"
         ))
         frappe.db.commit()
 
